05e269e97a0cf1ee6a608ba56c307c7d2525b48b
Jordan Bach <jordan.bach@2dialog.com>
2017-03-11 11:59:16 -0600

    choose better names

--- examples/s3-website/main.tf
+++ examples/s3-website/main.tf
@@ -24,40 +24,49 @@ variable "route53_zones" {
 }
 
 output "nameservers" {
-  value = "${module.s3_website_route53.nameservers}"
+  value = "${module.website_dns.nameservers}"
 }
+
+output "cdn_primary_endpoint" {
+  value = "${module.website_cdn_primary.domain_name}"
+}
+
+output "cdn_redirect_endpoint" {
+  value = "${module.website_cdn_redirects.domain_name}"
+}
+
 
 provider "aws" {
   region = "us-west-2"
 }
 
-module "s3_website_bucket" {
+module "website_bucket" {
   source = "github.com/jbgo/terraform-modules//s3-website-bucket"
   primary_domain = "${var.primary_domain}"
 }
 
-module "s3_website_cloudfront" {
+module "website_cdn_primary" {
   source = "github.com/jbgo/terraform-modules//s3-website-cloudfront"
-  bucket_domain = "${module.s3_website_bucket.bucket_domain}"
+  bucket_domain = "${module.website_bucket.bucket_domain}"
   alias_domains = ["${var.primary_domain}"]
 }
 
-module "s3_website_cloudfront_redirect" {
+module "website_cdn_redirects" {
   source = "github.com/jbgo/terraform-modules//s3-website-cloudfront"
-  bucket_domain = "${module.s3_website_bucket.bucket_domain}"
+  bucket_domain = "${module.website_bucket.bucket_domain}"
   redirect_to_primary = true
   alias_domains = "${var.redirect_domains}"
 }
 
-module "s3_website_route53" {
+module "website_dns" {
   source = "github.com/jbgo/terraform-modules//s3-website-route53"
 
   zones = "${var.route53_zones}"
   primary_domain = "${var.primary_domain}"
 
-  primary_distribution_domain = "${module.s3_website_cloudfront.domain_name}"
-  primary_distribution_zone_id = "${module.s3_website_cloudfront.hosted_zone_id}"
+  primary_distribution_domain = "${module.website_cdn_primary.domain_name}"
+  primary_distribution_zone_id = "${module.website_cdn_primary.hosted_zone_id}"
 
-  redirect_distribution_domain = "${module.s3_website_cloudfront_redirect.domain_name}"
-  redirect_distribution_zone_id = "${module.s3_website_cloudfront_redirect.hosted_zone_id}"
+  redirect_distribution_domain = "${module.website_cdn_redirects.domain_name}"
+  redirect_distribution_zone_id = "${module.website_cdn_redirects.hosted_zone_id}"
 }
